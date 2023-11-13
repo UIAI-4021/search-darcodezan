02ebbfabce5d803dbc4fc82cde5844e72816bceb
Pouya Rahimpour <mpouya.rhm9981@gmail.com>
2023-11-13 19:40:48 +0330

    cleanup

--- regression.py
+++ regression.py
@@ -1,101 +1,85 @@
+# import libraries
+import time
+import numpy as np
 import pandas as pd
+# import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
 
+# regression class
 class Regression:
-    n = m = p = nabla = 0
-    X = y = theta = []
     
-    def __init__(self, input_dataset, output_dataset, number_of_iterations, learning_rate):
-        self.X = input_dataset
-        self.y = output_dataset
-        self.m = len(self.X)
-        # add fake feature
-        for i in range(self.m):
-            self.X[i].append(1)
-        self.n = len(self.X[0])
-        self.p = number_of_iterations
-        self.nabla = learning_rate
-        self.theta = [0]*self.n
-        self.gradiant_decent()
-
-    def h(self, idx):
-        res = 0
-        for i in range(self.n):
-            res += self.X[idx][i]*self.theta[i]
-        return res
+    def __init__(self, number_of_iterations, learning_rate):
+        self.number_of_iterations = number_of_iterations
+        self.learning_rate = learning_rate
+        
+    def h(self, x):
+        return self.theta.dot(x)
 
     def J(self):
         res = 0
-        for i in range(self.m):
+        for i in range(self.n):
             res += (self.h(self.X[i])-self.y[i])**2
-        res /= 2*self.m
+        res /= 2*self.n
         return res
 
     def gradiant(self):
-        gradiant_vector = [0]*self.n
-        for j in range(self.n):
-            for i in range(self.m):
-                gradiant_vector[j] += (self.h(i)-self.y[i])*self.X[i][j]
-            gradiant_vector[j] /= self.m
-        return gradiant_vector
+        nabla = np.array(self.n)
+        Y = np.zeros(self.m)
+        for i in range(self.m):
+            Y[i] = (self.h(self.X[i])-self.y[i])/self.m
+        nabla = Y.dot(self.X)
+        if (np.linalg.norm(nabla) != 0):
+            nabla /= np.linalg.norm(nabla)
+        return nabla
         
     def gradiant_decent(self):
-        for i in range(self.p):
+        for k in range(self.number_of_iterations):
+            nabla = self.gradiant()
             for j in range(self.n):
-                self.theta[j] -= self.nabla*self.gradiant()[j]
-    
-    def predict(self, x):
-        if len(x) < self.n:
-            x.append(1)
-        res = 0
-        for i in range(self.n):
-            res += self.theta[i]*x[i]
-        return res
+                self.theta[j] -= self.learning_rate*nabla[j]
             
+    def run(self, input_dataset, output_dataset):
+        self.X = np.array(input_dataset)
+        self.y = np.array(output_dataset)
         
-    
-    
+        # m(number of data), n(number of features or dimentions)
+        self.m = self.X.shape[0]
+        self.n = self.X.shape[1]
+        self.theta = np.zeros(self.n)
+        self.gradiant_decent()
 
+    def test(self, X_test, Y_test):
+        X_test = np.array(X_test)
+        y_hat = np.zeros(len(X_test))
+        for i in range(len(X_test)):
+            y_hat[i] = self.h(X_test[i])
+        
+        with open("1-UIAI4021-PR1-Q2.txt", "a") as f:
+            f.write("Logs:\n")
+            f.write(f"MSE: {(mean_squared_error(y_hat, Y_test))}\n")
+            f.write(f"RMSE: {(mean_squared_error(y_hat, Y_test))**0.5}\n")
+            f.write(f"MAE: {mean_absolute_error(y_hat, Y_test)}\n")
+            f.write(f"R2: {r2_score(y_hat, Y_test)}\n")
 
+# read from csv file
+df = pd.read_csv("~/downloads/Flight_Price_Dataset_Q2.csv")
 
-# number_of_iterations = 1000
-# learning_rate = 0.1
-# input_dataset = [
-#     [1],
-#     [2],
-#     [3],
-#     [4],
-#     [5],
-#     [6]
-# ]
-# output_dataset = [1, 2, 3, 4, 5, 6]
-# reg = Regression(input_dataset, output_dataset, number_of_iterations, learning_rate)
-
-# test = []
-# for i in range(6, 10):
-#     test.append(reg.h([i]))
-
-# print(test)
-df = pd.read_csv("/home/pouya/downloads/Flight_Price_Dataset_Q2.csv")
-# lb = LabelEncoder()
-# df["Mapping"] = lb.fit_transform(df["class"])
-
-# df1 = pd.concat([df, departure_time_mapping], axis=1)
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+# some mappings for non-numeric data
 departure_time_mapping = {
-    "Early_Morning": 0,
-    "Morning": 1,
-    "Afternoon": 2,
-    "Night": 3, 
-    "Late_Night": 4
+    "Early_Morning": 1,
+    "Morning": 3,
+    "Afternoon": 4,
+    "Night": 2, 
+    "Late_Night": 0
 }
 stops_mapping = {
-    "zero": 0,
+    "zero": 2,
     "one": 1,
-    "two_or_more": 2
+    "two_or_more": 0
 }
 class_mapping = {
     "Economy": 0,
@@ -105,27 +89,42 @@ df["departure_time"] = df["departure_time"].map(departure_time_mapping)
 df["stops"] = df["stops"].map(stops_mapping)
 df["arrival_time"] = df["arrival_time"].map(departure_time_mapping)
 df["class"] = df["class"].map(class_mapping)
+
+# remove nan data and normalize it
 df = df.dropna()
 df = df.reset_index(drop=True)
-y = df["price"]
-X = df.drop("price", axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+sclr = MinMaxScaler()
+df_norm = pd.DataFrame(sclr.fit_transform(df), columns=df.columns)
 
-learning_rate = 0.1
-number_of_iterations = 10
-reg = Regression(X_train.values.tolist(), y_train.values.tolist(), number_of_iterations, learning_rate)
+# split data to test and train
+Y = df_norm["price"]
+X = df_norm.drop("price", axis=1)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
 
+# add fake feature for regression
+X_test["fake_feature"] = 1
+X_train["fake_feature"] = 1
 
-def test(X_test, y_test):
-    y_hat = [0]*len(X_test)
-    for i in range(len(X_test)):
-        y_hat[i] = reg.predict(X_test[i])
-        
-    print(y_hat)
-    print(mean_absolute_error(y_hat, y_test))
-    print(mean_squared_error(y_hat, y_test))
-    print(r2_score(y_hat, y_test))
-    
+learning_rate = 0.01
+number_of_iterations = 500
 
+start_time = time.time()
+reg = Regression(number_of_iterations, learning_rate)
+reg.run(X_train, Y_train)
 
-test(X_test.values.tolist(), y_test.values.tolist())
+# write to file
+with open("1-UIAI4021-PR1-Q2.txt", "w") as f:
+    f.write("PRICE = ")
+    splt = " + "
+    for i in range(reg.n):
+        col = df.columns[i]
+        if i == (reg.n)-1:
+            splt = ""
+            col = "bias"
+        f.write(f"{reg.theta[i]} * {col}")
+        f.write(splt)
+    f.write("\n")
+    f.write(f"Training Time: {round(time.time() - start_time, 6)}s\n\n")
+
+# results of our regression model
+reg.test(X_test, Y_test)
